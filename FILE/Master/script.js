3374829dd91f744c3990974b52a052b9ee1e31fd
anthony soon <anthonychansoon@gmail.com>
2019-09-19 15:08:11 -0700

    final

--- FILE/Master/script.js
+++ FILE/Master/script.js
@@ -1,4 +1,4 @@
-let specialCharacters = ["!","@","#","$","%"];
+let specialCharacters = ["!","@","#","$","%","^","&","*","(","(","?"];
 
 // Array of numeric characters to be included in password
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
